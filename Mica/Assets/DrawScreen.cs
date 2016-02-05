e2f3b8a2c0828feac92fbb183d17b8ccda895710
Christopher Cheape <cheapeone@gmail.com>
2016-02-05 01:01:55 -0500

    Initial Commit of unity prototype. Added 1d raytracer.

---
+++ Mica/Assets/DrawScreen.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawScreen: MonoBehaviour {
+
+	private GUITexture screen;
+	private Texture2D tex;
+	private GameObject myCamera;
+	public float fov;
+
+	// Use this for initialization
+	void Start () {
+		myCamera = GameObject.FindGameObjectWithTag("MainCamera");
+		screen = this.GetComponent("GUITexture") as GUITexture;
+		tex = new Texture2D(Screen.width,Screen.height);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tex = new Texture2D(Screen.width,Screen.height);
+
+		//get the pixels
+		for(int x = 0; x < Screen.width; x++){
+			for(int y = 0; y < Screen.height; y++){
+				Color color = myCamera.GetComponent<Raytrace>().Trace(x, (float)Screen.width, fov);
+				tex.SetPixel(x, y, color);
+			}
+		}
+		tex.Apply();
+		screen.texture = tex;
+
+		/*
+		string s = "";
+		foreach(Color c in horizontalStrip){
+			s = s + c.ToString() + " ";
+		}
+		Debug.Log(s);
+
+		Color[] finalPixels = new Color[Screen.width * Screen.height];
+
+		for(int y = 0; y < Screen.height; y++){
+			for(int x = 0; x < Screen.width; x++)
+				finalPixels[Screen.width * y] = horizontalStrip[x];
+		}
+			
+		tex.SetPixel
+
+		tex.Apply();
+		screen.texture = tex;
+		*/
+	}
+}
---
+++ Mica/Assets/Raytrace.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Raytrace : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Trace(0f,1000f,90f);
+	}
+
+	public Color Trace(float x, float width, float fov){
+		float k = Mathf.Tan(fov * Mathf.Deg2Rad/2f);
+
+		float xDir = (x - width/2f) * (2f * k)/width;
+		float rotation = Mathf.Atan(xDir) * Mathf.Rad2Deg;
+		Vector2 direction = Quaternion.AngleAxis(rotation, new Vector3(0,0,-1)) * transform.forward;
+		direction.Normalize();
+		Vector2 origin = transform.position;
+
+		//Debug.DrawRay(origin,direction);
+
+		Ray ray = new Ray(origin,direction);
+		RaycastHit[] hits = Physics.RaycastAll(ray);
+
+		if(hits.Length > 0){
+			//Find closest hit
+			RaycastHit closestHit = new RaycastHit();
+			float minDistance = Mathf.Infinity;
+
+			foreach(RaycastHit hit in hits){
+				//Debug.Log("Distance" + hit.distance);
+				if(hit.distance < minDistance){
+					minDistance = hit.distance;
+					closestHit = hit;
+				}
+			}
+
+			//get color off of hit
+			Renderer rend = closestHit.transform.GetComponent<Renderer>();
+			MeshCollider meshCollider = closestHit.collider as MeshCollider;
+			Texture2D tex = rend.material.mainTexture as Texture2D;
+
+			Color pixel = tex.GetPixelBilinear(closestHit.textureCoord.x, closestHit.textureCoord.y);
+
+			//Debug.Log("Color " + pixel);
+
+			return pixel;
+		}
+		else return new Color(0,0,0);
+
+	}
+}
